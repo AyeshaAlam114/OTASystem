9f3286edd4cd03176d65881a64e9952903f0c4b8
AyeshaAlam <ayeshanooralam114@gmail.com>
2025-03-19 09:29:04 +0500

    Install Required Packages & Generate Models from Existing Database

--- OTASystem.Data/Data/ApplicationDbContext.cs
+++ OTASystem.Data/Data/ApplicationDbContext.cs
@@ -1,13 +1,43 @@
-﻿using Microsoft.EntityFrameworkCore;
-using OTASystem.Data.Models;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using OTASystem.Data.Data.Models;
 
-namespace OTASystem.Data
+namespace OTASystem.Data.Data;
+
+public partial class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext()
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-
-        public DbSet<User> Users { get; set; }
-
     }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-RDAQRSN;Database=OTASystemDB;Trusted_Connection=True;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC07FF7D1F34");
+
+            entity.HasIndex(e => e.Username, "UQ__Users__536C85E4CF16C3C8").IsUnique();
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.PasswordHash).HasMaxLength(255);
+            entity.Property(e => e.Role).HasMaxLength(20);
+            entity.Property(e => e.Username).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
